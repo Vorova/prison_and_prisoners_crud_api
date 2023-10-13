2e9012e3f663606573422cd7a1edf4451780f832
vvvorova <vvvorova@gmail.com>
2023-10-13 15:39:11 +0500

    add PrisonerModel.java and PrisonerModel.java

---
+++ src/main/java/com/vorova/model/PrisonModel.java
@@ -0,0 +1,11 @@
+package com.vorova.model;
+
+import lombok.Data;
+
+@Data
+public class PrisonModel {
+
+    private Long id;
+    private String title;
+
+}
---
+++ src/main/java/com/vorova/model/PrisonerModel.java
@@ -0,0 +1,12 @@
+package com.vorova.model;
+
+import lombok.Data;
+
+@Data
+public class PrisonerModel {
+
+    private Long id;
+    private String name;
+    private Long prison_id;
+
+}

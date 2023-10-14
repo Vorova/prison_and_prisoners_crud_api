6a2bcf25e6e2f27c5e06a7ddebfb6a6101f38305
vvvorova <vvvorova@gmail.com>
2023-10-14 10:40:20 +0500

    add PrisonerService.java and this implemented methods - PrisonerServiceImpl.java

---
+++ src/main/java/com/vorova/service/impl/PrisonerServiceImpl.java
@@ -0,0 +1,50 @@
+package com.vorova.service.impl;
+
+import com.vorova.dao.PrisonerDao;
+import com.vorova.dao.impl.PrisonerDaoImpl;
+import com.vorova.model.PrisonerModel;
+import com.vorova.service.PrisonerService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PrisonerServiceImpl implements PrisonerService {
+
+    private static final PrisonerServiceImpl instance;
+    private static final PrisonerDao prisonerDao = PrisonerDaoImpl.getInstance();
+
+    static {
+        instance = new PrisonerServiceImpl();
+    }
+
+    public static PrisonerServiceImpl getInstance() {
+        return instance;
+    }
+
+    private PrisonerServiceImpl() {}
+
+    @Override
+    public void create(PrisonerModel prisoner) {
+        prisonerDao.persist(prisoner);
+    }
+
+    @Override
+    public void update(Long prisonerId, PrisonerModel prisoner) {
+        prisonerDao.update(prisonerId, prisoner);
+    }
+
+    @Override
+    public void delete(Long prisonerId) {
+        prisonerDao.delete(prisonerId);
+    }
+
+    @Override
+    public PrisonerModel findById(Long prisonerId) {
+        return Optional.of(prisonerDao.findById(prisonerId)).get().orElseThrow();
+    }
+
+    @Override
+    public List<PrisonerModel> findAll() {
+        return null;
+    }
+}

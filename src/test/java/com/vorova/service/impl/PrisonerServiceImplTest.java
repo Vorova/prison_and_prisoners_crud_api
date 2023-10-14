312c14d8bfa881bcf2e560a921a548816aa7eefc
vvvorova <vvvorova@gmail.com>
2023-10-14 13:23:45 +0500

    add PrisonerServiceImplTest.java

---
+++ src/test/java/com/vorova/service/impl/PrisonerServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.vorova.service.impl;
+
+import com.vorova.dao.impl.PrisonerDaoImpl;
+import com.vorova.model.PrisonerModel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.SQLException;
+
+@ExtendWith({
+    MockitoExtension.class
+})
+class PrisonerServiceImplTest {
+
+    @InjectMocks
+    PrisonerServiceImpl prisonerService;
+
+    @Mock
+    PrisonerDaoImpl prisonerDao;
+
+    @Test
+    @DisplayName("check callback for prisonerDao")
+    void createPositiveTest() {
+        PrisonerModel prisoner = new PrisonerModel();
+        Mockito.doNothing().when(prisonerDao).persist(prisoner);
+        prisonerService.create(prisoner);
+        Mockito.verify(prisonerDao).persist(prisoner);
+    }
+
+    @Test
+    void createNegativeTest() {
+        PrisonerModel prisoner = new PrisonerModel();
+        Mockito.doThrow(new SQLException()).doNothing().when(prisonerDao).persist(prisoner);
+
+        prisonerService.create(prisoner);
+        Mockito.verify(prisonerDao).persist(prisoner);
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
